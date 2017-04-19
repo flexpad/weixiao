dc7535ad288986441397e3c414957308a939dee2
qiaoc <flexpad2011@gmail.com>
2017-04-19 19:48:44 +0800

    ClassCourse model updated.

--- Addons/Weixiao/Model/WxyClassCourseModel.class.php
+++ Addons/Weixiao/Model/WxyClassCourseModel.class.php
@@ -7,16 +7,18 @@ use Think\Model;
  * Course模型
  */
 class WxyClassCourseModel extends Model{
-    public function addImport($data){
+    /*public function addImport($data){
         $res = $this->add ( $data );
         return $res;
-    }
+    }*/
 
     public function addCourse($data){
         $map ['token'] = $data['token'];
-        $map ['valid_date'] = $data['date'];
+        $map ['valid_date'] = $data['valid_date'];
         $map ['grade'] = $data['grade'];
         $map ['class_id'] = $data['class_id'];
+        $map ['course_type'] = $data['course_type'];
+        $map ['course_name'] = $data['course_name'];
         //$map ['comment'] = $data['comment'];
         //$map ['classdate'] = $data['classdate'];
         //var_dump($data);
