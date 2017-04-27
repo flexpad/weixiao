22a7f9973660dd9d7ed905a36bebfe4d157202bf
qiaoc <flexpad2011@gmail.com>
2017-04-27 16:16:08 +0800

    A view model for score notification.

---
+++ Addons/Weixiao/Model/WxyScoreNotifyViewModel.class.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiaoc
+ * Date: 2016/11/27
+ * Time: 15:08
+ */
+namespace Addons\Weixiao\Model;
+//use Think\Model;
+use Think\Model\ViewModel;
+
+class WxyScoreNotifyViewModel extends ViewModel {
+    public $viewFields = array(
+
+        'WxyScore' => array(
+            'token'     => 'token',
+            'id'        => 'id', 
+            'sid'       => 'sid', 
+            'courseid'  => 'courseid', 
+            'term'      => 'exam', 
+            'score'     => 'score', 
+            'score1'    => 'score1', 
+            'score2'    => 'score2', 
+            'score3'    => 'score3', 
+            'exmscore'  => 'socreStr', 
+            'classdate' =>  'classdate', 
+            'comment'   =>  'comment',
+            'studentno' => 'studentno'
+            //'_on' => 'WxyStudentCard.studentno = WxyScore.studentno AND WxyStudentCard.token = WxyScore.token'
+        ),
+
+        'WxyStudentCare'=>array(
+            'openid' => 'openid',
+            '_on'    => 'WxyScore.token = WxyStudentCare.token AND WxyScore.studentno = WxyStudentCare.studentno'),
+        
+        'WxyStudentCard'=>array('name'=>'student_name',
+            'name' => 'stuname',
+            '_on'=>'WxyStudentCare.sid = WxyStudentCard.id'), // To see changed to WxyStudentCard.id
+
+        'WxyClassCourse' => array (
+            'course_name' => 'course',
+            'teacher' => 'teacher',
+            '_on' => 'WxyScore.courseid = WxyClassCourse.id'
+        ),
+        /*
+        'WxyDailyTime' => array(
+            'arriveTime' => 'arriveTime',
+            'leaveTime' => 'leaveTime',
+            'state' => 'state',
+            'description' => 'description',
+            '_on' => 'WxyStudentCard.studentno = WxyDailyTime.studentID'
+        )
+        */
+
+    );
+}
