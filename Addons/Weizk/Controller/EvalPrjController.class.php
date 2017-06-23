11a7e4c0895738b1787e571977d7454c6908ba40
ZhuKe423 <mczk423@163.com>
2017-06-23 14:17:49 +0800

    Change for eval FID.

--- Addons/Weizk/Controller/EvalPrjController.class.php
+++ Addons/Weizk/Controller/EvalPrjController.class.php
@@ -1,87 +1,244 @@
 <?php
 
 namespace Addons\Weizk\Controller;
+
 use Home\Controller\AddonsController;
 
-class EvalPrjController extends AddonsController{
-
-    public function __construct() {
-        if (_ACTION == 'show') {
-            $GLOBALS ['is_wap'] = true;
-        }
-
-        parent::__construct ();
-        $this->model = $this->getModel('ZkEvalPrj');
+class EvalPrjController extends AddonsController {
+    function survey_question() {
+        $param ['survey_id'] = I ( 'id', 0, 'intval' );
+        $url = addons_url ( 'Weizk://EvalItem/lists', $param );
+        // dump($url);
+        redirect ( $url );
     }
-    /**
-     * 显示指定模型列表数据
-     */
-    public function lists()
-    {
-        $page = I('p', 1, 'intval'); // 默认显示第一页数据
+    function survey_answer() {
+        $param ['survey_id'] = I ( 'id', 0, 'intval' );
+        $url = addons_url ( 'Weizk://EvalItemAnswer/lists', $param );
+        redirect ( $url );
+    }
+    function preview() {
+        $id = I ( 'id', 0, 'intval' );
+        $url = U ( 'index', array (
+            'id' => $id
+        ) );
+        $this->assign ( 'url', $url );
+        $this->display ( SITE_PATH . '/Application/Home/View/default/Addons/preview.html' );
+    }
+    function survey() {
+        $id = I ( 'get.id', 0, 'intval' );
+        $num = I ( 'num', 1, 'intval' );
+        $token = get_token ();
+        $survey = D ( 'ZkEvalPrj' )->getSurveyInfo ( $id );
+        $list = D ( 'ZkEvalItem' )->getQuestionInfo ( $id );
+        if (IS_POST) {
+            $follow_id = $this->mid;
+            $question_id = I ( 'post.question_id', 0, 'intval' );
+            $answer = D ( 'ZkEvalItemAnswer' )->getAnswerInfo ( $id, $question_id, $follow_id );
 
-        // 解析列表规则
-        $list_data = $this->_get_model_list($this->model);
-
-        $grids = $list_data ['list_grids'];
-        $fields = $list_data ['fields'];
-
-        // 关键字搜索
-        $key = $this->model ['search_key'] ? $this->model ['search_key'] : 'title';
-        if (isset ($_REQUEST [$key])) {
-            $map [$key] = array(
-                'like',
-                '%' . htmlspecialchars($_REQUEST [$key]) . '%'
-            );
-            unset ($_REQUEST [$key]);
+            $data ['cTime'] = time ();
+            $data ['answer'] = serialize ( $_POST ['answer'] );
+            if ($answer) {
+                D ( 'ZkEvalItemAnswer' )->updateAnswer ( $id, $question_id, $follow_id, $data );
+            } else {
+                $data ['prj_id'] = $id;
+                $data ['token'] = $token;
+                $data ['item_id'] = $question_id;
+                $data ['uid'] = $follow_id;
+                $data ['openid'] = get_openid ();
+                M ( 'ZkEvalItemAnswer' )->add ( $data );
+                D ( 'ZkEvalItemAnswer' )->getAnswerInfo ( $id, $question_id, $follow_id, true );
+            }
+            $num = $num + 1;
         }
-        // 条件搜索
-        foreach ($_REQUEST as $name => $val) {
-            if (in_array($name, $fields)) {
-                $map [$name] = $val;
+        $question_id = I ( 'post.next_id', 0, 'intval' );
+        if ($question_id == '-1') {
+            redirect ( U ( 'finish', 'survey_id=' . $id ) );
+        }
+        if (empty ( $question_id )) {
+            $question = $list [0];
+            $next_id = isset ( $list [1] ['id'] ) ? $list [1] ['id'] : '-1';
+        } else {
+            foreach ( $list as $k => $vo ) {
+                if ($vo ['id'] == $question_id) {
+                    $question = $vo;
+                    $next_id = isset ( $list [$k + 1] ['id'] ) ? $list [$k + 1] ['id'] : '-1';
+                }
             }
         }
 
-        $row = empty ($this->model ['list_row']) ? 20 : $this->model ['list_row'];
+        $extra = parse_config_attr ( $question ['extra'] );
+        $this->assign ( 'survey', $survey );
+        $this->assign ( 'question', $question );
+        $this->assign ( 'next_id', $next_id );
+        $this->assign ( 'extra', $extra );
+        $this->assign ( 'num', $num );
 
-        // 读取模型数据列表
-        empty ($fields) || in_array('id', $fields) || array_push($fields, 'id');
-        $name = parse_name(get_table_name($this->model ['id']), true);
+        $this->display ();
+    }
+    function index() {
+        $id = $map ['id'] = I ( 'id', 0, 'intval' );
+//		$openid = get_openid ();
+        $map ['token'] = get_token ();
+        $public_info = get_token_appinfo ( $map ['token'] );
+        $overtime = $this->_is_overtime ( $id );
+        //	$overtime = $overtime ? 1 :( $overtime ? 2 : 0 ) ;
 
 
-        $data = M($name)->field(empty ($fields) ? true : $fields)->where($map)->order('id')->page($page, $row)->select();
+// 		if($overtime= 1)$status="调研进行中";
+// 		if($overtime= 2)$status="调研未开始";
+// 		if($overtime= 0)$status="调研已结束";
+        $this->assign ( 'overtime', $overtime );
+// 		$this->assign ( 'status', $status);
 
-        /* 查询记录总数 */
-        $count = M($name)->where($map)->count();
+        $info = M ( 'ZkEvalPrj' )->where ( $map )->find ();
+        $this->assign ( 'info', $info );
+        $this->assign ( 'public_info', $public_info );
+        $this->display ();
+    }
+    function finish() {
+        $survey_id = I ( 'survey_id', 0, 'intval' );
+        // $map ['token'] = get_token ();
+        // $info = M ( 'survey' )->where ( $map )->find ();
+        $info = D ( 'ZkEvalPrj' )->getSurveyInfo ( $survey_id );
+        $this->assign ( 'info', $info );
 
-        if ($count > $row) {
-            $page = new \Think\Page ($count, $row);
-            $page->setConfig('theme', '%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%');
-            $this->assign('_page', $page->show());
-        }
-        $this->assign('list_grids', $grids);
-        $this->assign('list_data', $data);
-
-        $this->display();
+        // 增加积分
+        //add_credit ( 'survey' );
+        $this->display ();
     }
 
-    public function add(){
-        if(IS_POST){
-            $data['name'] =  trim(I('post.name'));
-            $data['item_list'] = json_encode(explode(',',trim(I('post.item_list'))));
-            $data['timestamp'] = time();
-            $tmp_model = D('ZkEvalPrj');
-            $res = $tmp_model->create($data);
-            if($res)
-                $this->success('新增保存成功！', U ( 'lists'), 600);
-            else
-                $this->error('添加失败');
+    // 已过期返回 true ,否则返回 false
+    private function _is_overtime($survey_id) {
+        // 先看调研期限过期与否
+        $the_survey = M ( "ZkEvalPrj" )->find ( $survey_id );
 
+        if ((! empty ( $the_survey ['start_time'] )) && $the_survey ['start_time'] < NOW_TIME && $the_survey ['end_time'] > NOW_TIME)
+
+            return 1; //进行中
+
+        // $deadline = $the_survey ['end_date'] + 86400;
+        if ((! empty ( $the_survey ['start_time'] )) && $the_survey ['start_time'] > NOW_TIME )
+
+            return 2; //未开始
+
+        if ((! empty ( $the_survey ['start_time'] )) &&  $the_survey ['end_time'] < NOW_TIME)
+            return 0;  //结束
+
+
+
+    }
+    function lists() {
+        $isAjax = I ( 'isAjax' );
+        $isRadio = I ( 'isRadio' );
+        $model = $this->getModel ( 'ZkEvalPrj' );
+        $list_data = $this->_get_model_list ( $model, 0, 'id desc', true );
+        // 判断该活动是否已经设置投票调查
+        if ($isAjax) {
+            $this->assign ( 'isRadio', $isRadio );
+            $this->assign ( $list_data );
+            $this->display ( 'ajax_lists_data' );
+        } else {
+            $this->assign ( $list_data );
+            $this->display ();
         }
-        else{
-            $this->display("add");
+    }
+    function add() {
+        $this->display ( 'edit' );
+    }
+    function edit() {
+        $id = I ( 'id', 0, 'intval' );
+        $model = $this->getModel ( 'ZkEvalPrj' );
+
+        if (IS_POST) {
+            //$this->checkDate();
+            $act = empty ( $id ) ? 'add' : 'save';
+            $Model = D ( parse_name ( get_table_name ( $model ['id'] ), 1 ) );
+            // 获取模型的字段信息
+            $Model = $this->checkAttr ( $Model, $model ['id'] );
+            $res = false;
+            $Model->create () && $res = $Model->$act ();
+            if ($res !== false) {
+                $act == 'add' && $id = $res;
+
+                $this->_setAttr ( $id, $_POST );
+                D ( 'Common/Keyword' )->set ( I ( 'post.keyword' ), 'Weizk', I ( 'post.id' ) );
+                $this->success ( '保存成功！', U ( 'lists?model=' . $model ['name'], $this->get_param ) );
+            } else {
+                $this->error ( $Model->getError () );
+            }
+        } else {
+            // 获取数据
+            $data = M ( get_table_name ( $model ['id'] ) )->find ( $id );
+            $data || $this->error ( '数据不存在！' );
+
+            $token = get_token ();
+            if (isset ( $data ['token'] ) && $token != $data ['token'] && defined ( 'ADDON_PUBLIC_PATH' )) {
+                $this->error ( '非法访问！' );
+            }
+            $this->assign ( 'data', $data );
+
+            // 字段信息
+            $map ['prj_id'] = $id;
+            $map ['token'] = $token;
+            $list = M ( 'ZkEvalItem' )->where ( $map )->order ( 'id' )->select ();
+
+            $this->assign ( 'attr_list', $list );
+
+            $this->display ( 'edit' );
+        }
+    }
+    // 保存字段信息
+    function _setAttr($forms_id, $data) {
+        $dao = M ( 'ZkEvalItem' );
+        $save ['prj_id'] = $forms_id;
+
+        $old_ids = $dao->where ( $save )->getFields ( 'id' );
+
+        $sort = 0;
+        foreach ( $data ['attr_title'] as $key => $val ) {
+            $save ['title'] = safe ( $val );
+            if (empty ( $save ['title'] ))
+                continue;
+
+            $save ['extra'] = safe ( $data ['extra'] [$key] );
+            $save ['type'] = safe ( $data ['type'] [$key] );
+            $save ['is_must'] = intval ( $data ['is_must'] [$key] );
+            $save ['value'] = safe ( $data ['value'] [$key] );
+            $save ['remark'] = safe ( $data ['remark'] [$key] );
+            $save ['validate_rule'] = safe ( $data ['validate_rule'] [$key] );
+            $save ['error_info'] = safe ( $data ['error_info'] [$key] );
+            $save ['sort'] = $sort;
+
+            $id = intval ( $data ['attr_id'] [$key] );
+            if (! empty ( $id )) {
+                $ids [] = $map ['id'] = $id;
+                $dao->where ( $map )->save ( $save );
+            } else {
+                $save ['token'] = get_token ();
+                $ids [] = $dao->add ( $save );
+            }
+
+            $sort += 1;
         }
 
+        $diff = array_diff ( $old_ids, $ids );
+        if (! empty ( $diff )) {
+            $map2 ['id'] = array (
+                'in',
+                $diff
+            );
+            $dao->where ( $map2 )->delete ();
+        }
+    }
+
+    function checkDate(){
+        // 判断时间选择是否正确
+        if (! I ( 'post.start_time' )) {
+            $this->error ( '请选择开始时间' );
+        } else if (! I ( 'post.end_time' )) {
+            $this->error ( '请选择结束时间' );
+        } else if (strtotime ( I ( 'post.start_time' ) ) >= strtotime ( I ( 'post.end_time' ) )) {
+            $this->error ( '开始时间不能大于或等于结束时间' );
+        }
     }
 }
-
