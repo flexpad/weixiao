df14b0a08b2d6544a3ea4220a0e47578f59b1c2e
djrockboy@qq.com <dwdm1830>
2017-08-10 16:42:17 +0800

    Change for hschool wap

--- Addons/Student/Model/WxyStudentCardModel.class.php
+++ Addons/Student/Model/WxyStudentCardModel.class.php
@@ -16,42 +16,18 @@ class WxyStudentCardModel extends Model{
         $this->studentManMode = M('WxyStudentCardManage');
     }
 
-    public function addStudent($data, $timeStamp){
+    public function addStudent($data){
         $map ['token'] = $data['token'];
         $map ['studentno'] = $data['studentno'];
         $map['name'] = $data['name'];
         //var_dump($map);
         //var_dump($this->where[$map]);
 
-        $stuManData['name'] = $data['name'];
-        $stuManData['studentno'] = $data['studentno'];
-        $stuManData['cardno'] = $data['cardno'];
-        $stuManData['timestamp'] = $timeStamp;
-        $stuManData['pri'] = '0';
-        $stuManData['group'] = '1';
-
-        $dbData = $this->where($map)->select();
-
-/*        var_dump('data = ', $data);
-        var_dump('---- dbData = ', $dbData);*/
-        if ($dbData != NULL)
-        {
-            if(($data['cardno'] != NULL) &&
-                ($data['cardno'] != $dbData[0]['cardno']))
-            {
-                $this->where($map)->save($data);
-                $stuManData['operation'] = ($dbData[0]['cardno']==NULL)?'add':'change';
-                $this->studentManMode->delete();
-                $this->studentManMode->add($stuManData);
-            }
+        if ($this->where($map)->select() != NULL)
             return false;
-        }
         else
         {
             $res = $this->add ( $data );
-            $stuManData['operation'] = 'add';
-            $this->studentManMode->delete();
-            $this->studentManMode->add($stuManData);
             return $res;   
         }           
     }
